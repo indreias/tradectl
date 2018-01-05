212e55b66111917dac966681a56cccd392644e84
Ioan Indreias <ioan.indreias@axigen.com>
2018-01-05 19:40:09 +0200

    Fix output with numeric sort; Fix output for hidden bid orders

--- tradeville/getMarketDepth.sh
+++ tradeville/getMarketDepth.sh
@@ -18,8 +18,15 @@ info=$(cat "$OUT_TMP" | sed '
 s|<table.*</th></tr>||g;s|<tr class="total".*$||;
 s|<tr[^>]*>|\n|g;
 s|<td[^>]*>||g;
-s|<hr/>\([^<]*\)</td>\([^<]*\)</td>|:\2 (\1)|g;
-s|</td>|:|g;s|,||g' | awk 'BEGIN{FS=":"}{if ($1 != "") {print "b "$3" "$2" "$1}}{if ($4 != "") {print "a "$4" "$5" "$6}}' | sort --key=2,2
+s|<hr/>\([^<]*\)</td>\([^<]*\)</td>|:\2 (\1):|g;
+s|</td>|:|g;s|,||g' | awk '
+BEGIN{FS=":"}
+{if ($2 != "")
+    {print "b "$3" "$2" "$1}
+ else
+    {if ($1 != "") {print $1}}
+}
+{if ($4 != "") {print "a "$4" "$5" "$6}}' | sort -n --key=2,2 | sed 's|\(.*\)\( (.*)\)\(.*\)|\1\3\2|'
 )
 
 if [ -n "$info" ]
