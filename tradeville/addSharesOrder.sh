3c3a3c1fd6cfd3cdd270ce6db374e9a1959a3be4
Ioan Indreias <ioan.indreias@axigen.com>
2018-01-05 18:01:56 +0200

    Add support for day, date and minutes order valability

--- tradeville/addSharesOrder.sh
+++ tradeville/addSharesOrder.sh
@@ -3,25 +3,33 @@
 . "$(dirname $0)/../tradeville.conf"
 
 HELPME_VAR=4
-HELPME_TXT="<orderType> <shareSymbol> <price> <quantity>
+HELPME_TXT="buy|sell <shareSymbol> <price> <quantity> [open|day|<mm/dd/yyyy>|<minutes>]
 Add new order for the mentioned share.
-Available orderType are:
-    * buy 
-    * sell
 
-Note: currently only OPEN orders are supported"
+Note: The default value for valability is open."
 helpMe $@
 
 case "$1" in
   buy)	action=C;;
   sell)	action=V;;
-  *)	exitOnError 1 "orderType '$1' not supported yet";;
+  *)	exitOnError 1 "orderType '$1' is not supported yet";;
 esac
 
 shareSymbol=$2
 price=$3
 quantity=$4
-valability=$(date '+%m/%d/%Y')
+
+valabValue=$(date '+%m/%d/%Y')
+shopt -s extglob
+case "$5" in
+  open)		valabType=open;; 
+  day)		valabType=day;;
+  0[1-9]/[0-3][0-9]/2[0-9][0-9][0-9])	valabType=date;valabValue=$5;;
+  1[0-2]/[0-3][0-9]/2[0-9][0-9][0-9])	valabType=date;valabValue=$5;;
+  ([1-9]*([0-9]))	valabType=min;valabValue=$5;;
+  *)		exitOnError 2 "valabilityType '$5' is not supported yet";;
+esac
+shopt -u extglob
 
 if isLoggedIn
 then
@@ -36,7 +44,7 @@ myAccountID=$(OFFLINE=1 "$DIR_SELF/getAccountID.sh")
 
 if [ -n "$newOrderGUID" ]
 then
-  info=$(my_curl "$MY_URL/UpdateAjax/Utils.ashx?method=AddSharesOrder&symbol=$shareSymbol&hasDetails=0&obs=&isHidden=0&smsAlertChecked=0&tradingAccount=$myAccountID&atMarket=0&price=$price&action=$action&allQuantity=0&quantity=$quantity&valabType=open&valabValue=$valability&conditionType=0&conditionValue=&isCondOrder=0&hidNewOrderGUID=$newOrderGUID&visibleQuantity=0&validMode=0&uniqueParam=$(date '+%s')" -o "$OUT_TMP")
+  info=$(my_curl "$MY_URL/UpdateAjax/Utils.ashx?method=AddSharesOrder&symbol=$shareSymbol&hasDetails=0&obs=&isHidden=0&smsAlertChecked=0&tradingAccount=$myAccountID&atMarket=0&price=$price&action=$action&allQuantity=0&quantity=$quantity&valabType=$valabType&valabValue=$valabValue&conditionType=0&conditionValue=&isCondOrder=0&hidNewOrderGUID=$newOrderGUID&visibleQuantity=0&validMode=0&uniqueParam=$(date '+%s%N' | cut -c -13)" -o "$OUT_TMP")
   msg="$newOrderGUID|"$(cat "$OUT_TMP" | sed 's/\..*/\./')
 else
   msg="FAILURE\tcode:99"
