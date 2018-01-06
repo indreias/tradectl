01fff8b640c7d7e09d7ee0b657ea3d33225f9dd4
Ioan Indreias <ioan.indreias@axigen.com>
2018-01-06 18:22:27 +0200

    Add support for dateStart and dateStop

--- tradeville/getQuotes.sh
+++ tradeville/getQuotes.sh
@@ -7,11 +7,12 @@
 . "$(dirname $0)/../tradeville.conf"
 
 HELPME_VAR=0
-HELPME_TXT="[<shareSymbol>] [<dataType>]
+HELPME_TXT="[<shareSymbol>] [<dataType>] [<dateStart>] [<dateStop>]
 Get quotes for the mentioned share (or for all if not mentioned).
 Available datatypes are:
     * AVG  - daily averages (default type)
-    * TRAN - daily tranzactions"
+    * TRAN - daily tranzactions
+Note: if start date or stop date are not provided (mm/dd/yyyy format) them will be considered today"
 helpMe $@
 
 if isLoggedIn
@@ -24,12 +25,26 @@ else
   dataType=$2
 fi
 
+if [ -z "$3" ]
+then
+  dateStart=$(date '+%m/%d/%Y')
+else
+  dateStart=$3
+fi
+
+if [ -z "$4" ]
+then
+  dateStop=$(date '+%m/%d/%Y')
+else
+  dateStop=$4
+fi
+
 case $dataType in
   TRAN)	hData="data si ora tran";;
   *)    hData="data tran";;
 esac
 
-info=$(my_curl "$MY_URL/Trading/ReportsAndQuotes/DownloadMetaStockDisplay.aspx?DateStart=11/01/2017&DateStop=12/03/2017&AccountType=A&Symbol=$1&Ajusted=Y&Format=TXT&DataType=$dataType" -o - 2>&1)
+info=$(my_curl "$MY_URL/Trading/ReportsAndQuotes/DownloadMetaStockDisplay.aspx?DateStart=$dateStart&DateStop=$dateStop&AccountType=A&Symbol=$1&Ajusted=Y&Format=TXT&DataType=$dataType" -o - 2>&1)
 ret_code=$?
 exitOnError $ret_code "$info"
 
